75bf85a5d69c90f178dbaa28783b0b6a542dbb6b
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2021-05-25 13:41:25 +0000

    userのshow,edit,update実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '78c4574f7ac00ed8e913e5324ce7c15c2a550ccbb7382edfc6838a48779670cab203a70544ba631b6e5bea5a6d5d5ff862d88549ebf0c050645075895724aaed'
